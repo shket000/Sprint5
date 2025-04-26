e29a55dc5ae75f3680b90bdd9f48b18722309879
000 <p04tabiz@yandex.ru>
2025-04-26 22:31:22 +0300

    1

--- internal/trainings/trainings.go
+++ internal/trainings/trainings.go
@@ -5,7 +5,22 @@ type Training struct {
 }
 
 func (t *Training) Parse(datastring string) (err error) {
-	// TODO: реализовать функцию
+	s1 := strings.Split(datastring, ",")
+	if len(s1) != 3 {
+		return fmt.Errorf("data string does not contain 3 elements")
+	}
+	t.Steps, err = strconv.Atoi(s1[0])
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	t.TrainingType = s1[1]
+	t.Duration, err = time.ParseDuration(s1[2])
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
 }
 
 func (t Training) ActionInfo() (string, error) {
